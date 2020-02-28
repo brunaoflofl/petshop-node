4f03a4081809f9d9b15e008ba93a3a3eedcdc2e5
solfalcao <silmarasol@hotmail.com>
2020-02-28 17:43:29 -0300

    petshop-func

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const  http = require('http');
+const  http = require('http')
+const url = require('url')
+const adcPets = require('./petshop')
 http.createServer(function(req, res){
-    res.writeHead(200, {"Content-Type": "text/plain"})
-    res.end("Rodando...")
+    res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"})
+    if(req.url == '/'){
+        res.end("Rodando...")
+    }
+    
+    if(req.url == '/home'){
+        res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"})
+        res.end("Você está dentro do sistema petShop!")
+    }
+   /* if(req.url == '/pet/adicionar'){
+        petshop.adcPets('dodoli')
+            res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"})
+            res.end("Pet adicionado com sucesso!")
+        
+        
+        
+        
+    }*/
 }).listen(3000, 'localhost'); 
---
+++ petshop.js
@@ -0,0 +1,11 @@
+let listaDePets = []
+
+function adcPets(nome){
+   listaDePets.push(nome)
+   console.log(listaDePets)
+   return true
+}
+module.exports = {adcPets}
+
+//fazer uma funcao listar pets
+//funcao q  busque um pet pelo nome 
